564c9143a2a9475a570d7a64f4ccf9eab22f7fe6
kiichiro-tominaga <kiichiro-tominaga@kccs.co.jp>
2026-03-26 01:03:22 +0900

    feat: add Terraform infra, K8s manifests, and CI/CD pipeline
    - Terraform: VPC, EKS, DynamoDB, ECR, IRSA, ALB Controller
    - K8s: Deployment, Service, Ingress, ServiceAccount with Kustomize
    - ArgoCD: GitOps application with auto-sync
    - CI: Go build/test on PR via GitHub Actions + flox
    - CD: ECR push + manifest update on merge to main

---
+++ infra/variables.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-northeast-1"
+}
+
+variable "project" {
+  description = "Project name"
+  type        = string
+  default     = "url-shortener"
+}
+
+variable "cluster_version" {
+  description = "EKS cluster version"
+  type        = string
+  default     = "1.32"
+}
